87c1f343e4f32f3d43c77cb15e24cc6a0affc82f
DESKTOP-IUH4BPV\Henock <henockmamo54@gmail.com>
2017-11-09 15:48:03 +0900

    project created

---
+++ socket programming/Client/Client/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        static Socket sock;
+        public Form1()
+        {
+            InitializeComponent();
+
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+            try {
+                sock.Connect(localEndPoint);
+            }
+            catch (Exception) {
+                Console.Write("unable to connect");
+                
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes("hi heni");
+
+            sock.Send(data);
+
+            sock.Close();
+        }
+    }
+}
---
+++ socket programming/Server/server/server/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+        static byte[] Buffer { get; set; }
+        static Socket sock;
+        Socket acceptedSock;
+        public Form1()
+        {
+            InitializeComponent();
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(new IPEndPoint(0,1234));
+            sock.Listen(100);
+
+            acceptedSock = sock.Accept();
+            Buffer = new byte[acceptedSock.SendBufferSize];
+            int bytesRead = acceptedSock.Receive(Buffer);
+
+            byte[] formatted = new byte[bytesRead];
+
+            for (int i = 0; i < bytesRead; i++) {
+                formatted[i] = Buffer[i];
+            }
+
+            string strData = Encoding.ASCII.GetString(formatted);
+            Console.Write(strData + "\r\n");
+            Console.Read();
+            sock.Close();
+            acceptedSock.Close();
+
+        }
+    }
+}
